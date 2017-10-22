0881ba95775824e89519067baad53cc5d9a9c9e7
qmilan <qmilan911@gmail.com>
2017-10-22 15:34:11 +0400

    Подключение тестового фреймворка TestNG и создание тестов для класса Point

--- sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
+++ sandbox/src/main/java/ru/stqa/pft/sandbox/MyFirstProgram.java
@@ -2,8 +2,8 @@ package ru.stqa.pft.sandbox;
 
 public class MyFirstProgram {
   public static void main(String[] args) {
-    Point p1 = new Point(3,3);
-    Point p2 = new Point(7,6);
+    Point p1 = new Point(0,0);
+    Point p2 = new Point(4,3);
     System.out.println("Расстояние между точками p1 и p2" + " = "+ distance(p1,p2));
     System.out.println("Расстояние между точками p1 и p2" + " = "+ p2.distance(p1));
     System.out.println("Расстояние между точками p1 и p2" + " = "+ p1.distance(p2));
---
+++ sandbox/src/test/java/ru/stqa/pft/sandbox/PointTests.java
@@ -0,0 +1,28 @@
+package ru.stqa.pft.sandbox;
+
+import org.testng.Assert;
+import org.testng.annotations.Test;
+
+public class PointTests {
+//доп.проверка
+    @Test
+    public void testDistance () {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(4,3);
+        Assert.assertEquals(MyFirstProgram.distance(p1,p2),5.0);
+    }
+//проверка в точке p1
+    @Test
+    public void p1Distance () {
+        Point p1 = new Point(0,0);
+        Point p2 = new Point(8,0);
+        Assert.assertEquals(p1.distance(p2),8.0);
+    }
+//проверка в точке p2
+    @Test
+    public void p2Distance () {
+        Point p1 = new Point(3,3);
+        Point p2 = new Point(7,6);
+        Assert.assertEquals(p2.distance(p1),5.0);
+    }
+}
