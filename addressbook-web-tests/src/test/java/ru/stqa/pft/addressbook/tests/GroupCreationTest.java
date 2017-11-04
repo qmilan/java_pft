cece5b3ded5666daee50f68612bc5b307eec4eca
qmilan <qmilan911@gmail.com>
2017-11-04 19:43:16 +0400

    Реализована проверка  и обеспечение предусловий групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupCreationTest.java
@@ -9,10 +9,7 @@ public class GroupCreationTest extends TestBase {
   @Test
   public void testGroupCreationTest() {
     app.getNavigationHelper().goToGroupPage();
-    app.getGroupHelper().initGroupCreation();
-    app.getGroupHelper().fillGroupForm(new GroupData("test1", null, null));
-    app.getGroupHelper().submitGroupCreation();
-    app.getGroupHelper().returnToGroupPage();
+    app.getGroupHelper().createGroup(new GroupData("test1", null, null));
 
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -7,6 +7,9 @@ public class GroupModificationTest extends TestBase {
   @Test
   public void testGroupModificationTest (){
     app.getNavigationHelper().goToGroupPage();
+    if (! app.getGroupHelper().isThereAGroup ()){
+      app.getGroupHelper().createGroup (new GroupData("test1", null, null));
+    }
     app.getGroupHelper().selectGroup();
     app.getGroupHelper().initGroupModification();
     app.getGroupHelper().fillGroupForm(new GroupData("test1", "test2", "test3"));
