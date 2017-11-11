402f07538dc82d8787c14e3a6330d3821fb416a0
qmilan <qmilan911@gmail.com>
2017-11-12 00:59:53 +0400

    Проверка множества списка контактов -верно

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/ContactModificationTest.java
@@ -23,7 +23,7 @@ public class ContactModificationTest extends TestBase {
     List<ContactData> before =app.getContactHelper().getContactList();
     app.getContactHelper().selectContact(before.size()-1);
     app.getContactHelper().initContactModification();
-    ContactData contact = new ContactData(before.get(before.size()-1).getId(),"test1", "test2", "test3", "test4@test.com", "89991234567", null);
+    ContactData contact = new ContactData(before.get(before.size()-1).getId(),"test2", "test2", "test3", "test4@test.com", "89991234567", null);
     app.getContactHelper().fillContactCreation(contact, false);
     app.getContactHelper().submitContactModification();
     app.getContactHelper().returnToHomePage();
