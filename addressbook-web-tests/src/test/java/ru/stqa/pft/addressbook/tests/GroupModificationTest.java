f84c9485101f788ceefc61dfd4e6fa7ccaa0107c
qmilan <qmilan911@gmail.com>
2017-11-19 20:52:50 +0400

    Уникальный идентификатор объектов  для групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/GroupModificationTest.java
@@ -5,34 +5,29 @@ import org.testng.annotations.BeforeMethod;
 import org.testng.annotations.Test;
 import ru.stqa.pft.addressbook.model.GroupData;
 
-import java.util.Comparator;
 import java.util.HashSet;
-import java.util.List;
+import java.util.Set;
 
 public class GroupModificationTest extends TestBase {
   @BeforeMethod
   public void ensurePrecondition (){
     app.goTo().groupPage();
-    if (app.group().list().size()==0) {
+    if (app.group().all().size()==0) {
       app.group().create(new GroupData().withName("test1"));
     }
   }
   @Test
   public void testGroupModificationTest() {
-    List<GroupData> before = app.group().list();
-    int index = before.size() - 1;
-    GroupData group = new GroupData().withId(before.get(index).getId()).withName("test1")
+    Set<GroupData> before = app.group().all();
+    GroupData modifyGroup = before.iterator().next();
+    GroupData group = new GroupData().withId(modifyGroup.getId()).withName("test1")
             .withHeader("test2").withFooter("test3");
-    app.group().modify(index, group);
-    List<GroupData> after = app.group().list();
+    app.group().modify(group);
+    Set<GroupData> after = app.group().all();
     Assert.assertEquals(after.size(), before.size());
-    before.remove(index);
+    before.remove(modifyGroup);
     before.add(group);
     Assert.assertEquals(new HashSet<Object>(before), new HashSet<Object>(after));
-
-    Comparator<? super GroupData> byId = (g1, g2) -> Integer.compare(g1.getId(), g2.getId());
-    before.sort(byId);
-    after.sort(byId);
     Assert.assertEquals(before, after);
   }
 
