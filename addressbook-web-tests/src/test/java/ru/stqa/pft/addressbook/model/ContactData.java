c31f55deba23d784e54306abe626bd24531c2a11
qmilan <qmilan911@gmail.com>
2017-12-02 19:53:16 +0400

    Извлечение данных из БД для контактов и групп

--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/model/ContactData.java
@@ -2,32 +2,59 @@ package ru.stqa.pft.addressbook.model;
 
 import com.thoughtworks.xstream.annotations.XStreamAlias;
 import com.thoughtworks.xstream.annotations.XStreamOmitField;
+import org.hibernate.annotations.Type;
 
+import javax.persistence.*;
 import java.io.File;
 @XStreamAlias("contact")
+@Entity
+@Table(name = "addressbook")
 public class ContactData {
   @XStreamOmitField
+  @Id
+  @Column(name = "id")
   private int id =Integer.MAX_VALUE;
+  @Column(name = "firstname")
   private String firstname;
+  @Column(name = "lastname")
   private String lastname;
+  @Column(name = "address")
+  @Type(type = "text")
   private String address;
+  @Column(name = "email")
+  @Type(type = "text")
   private String email;
+  @Column(name = "email2")
+  @Type(type = "text")
   private String email2;
+  @Column(name = "email3")
+  @Type(type = "text")
   private String email3;
+  @Column(name = "mobile")
+  @Type(type = "text")
   private String mobile;
+  @Column(name = "home")
+  @Type(type = "text")
   private String home;
+  @Column(name = "work")
+  @Type(type = "text")
   private String work;
+  @Transient
   private String group;
+  @Transient
   private String allPhones;
+  @Transient
   private String allEmails;
-  private File photo;
+  @Column(name = "photo")
+  @Type(type = "text")
+  private String photo;
 
   public File getPhoto() {
-    return photo;
+    return new File (photo);
   }
 
   public ContactData withPhoto(File photo) {
-    this.photo = photo;
+    this.photo = photo.getPath();
     return this;
   }
 
--- addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/HbConnectionTest.java
@@ -7,6 +7,7 @@ import org.hibernate.boot.registry.StandardServiceRegistry;
 import org.hibernate.boot.registry.StandardServiceRegistryBuilder;
 import org.testng.annotations.BeforeClass;
 import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
 import ru.stqa.pft.addressbook.model.GroupData;
 
 import java.util.List;
@@ -37,6 +38,17 @@ public class HbConnectionTest {
     List<GroupData> result = session.createQuery( "from GroupData" ).list();
     for ( GroupData group : (List<GroupData>) result ) {
       System.out.println( group);
+    }
+    session.getTransaction().commit();
+    session.close();
+  }
+  @Test
+  public void testHbConnectionContact () {
+    Session session = sessionFactory.openSession();
+    session.beginTransaction();
+    List<ContactData> result = session.createQuery( "from ContactData where deprecated = '0000-00-00'" ).list();
+    for ( ContactData contact : (List<ContactData>) result ) {
+      System.out.println( contact);
     }
     session.getTransaction().commit();
     session.close();
