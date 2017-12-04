b9a2ada949d7d2b6e2e419d8580bd5e6ee9147a3
qmilan <qmilan911@gmail.com>
2017-12-04 20:35:35 +0400

    Задание 16 попытка 1

---
+++ addressbook-web-tests/src/test/java/ru/stqa/pft/addressbook/tests/AddContactInGroup.java
@@ -0,0 +1,33 @@
+package ru.stqa.pft.addressbook.tests;
+
+import org.testng.annotations.BeforeMethod;
+import org.testng.annotations.Test;
+import ru.stqa.pft.addressbook.model.ContactData;
+import ru.stqa.pft.addressbook.model.GroupData;
+import ru.stqa.pft.addressbook.model.Groups;
+
+public class AddContactInGroup extends TestBase {
+  @BeforeMethod
+  public void ensurePrecondition() {
+    if (app.db().contacts().size() == 0) {
+      Groups beforegroup = app.db().groups();
+      if (beforegroup.size()==0){
+        app.goTo().groupPage();
+        app.group().create(new GroupData().withName("test1"));
+      }
+      Groups aftergroup = app.db().groups();
+      GroupData idGroups = aftergroup.iterator().next();
+      ContactData contact = new ContactData().withFirstname("test1").withLastname("test2").withAddress("test3")
+              .withEmail("test@test.com").withEmail2("test1@test.com")
+              .withEmail3("test2@test.com").withMobile("89501234567").withHome("123445")
+              .withWork("3412111").inGroup(idGroups);
+//      if (!listsGroupFromBD.stream().anyMatch(g -> g.getName().equals(groupName))) {
+      app.goTo().contactCreationPage();
+      app.contact().create(contact, true);
+    }
+  }
+  @Test
+  public void testAddContactInGroup (){
+
+  }
+}
