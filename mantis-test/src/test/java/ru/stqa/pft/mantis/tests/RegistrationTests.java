6074dd59e6d4f3ea28b5ae538fc224fa82b0b465
qmilan <qmilan911@gmail.com>
2017-12-12 22:15:54 +0400

    ftp не работает

--- mantis-test/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/tests/RegistrationTests.java
@@ -6,6 +6,6 @@ public class RegistrationTests extends TestBase {
 
   @Test
   public void testRegistration() {
-    app.registration().start ("user1","user1@localhost");
+    app.registration().start ("user1","user1@localhost.localdomain");
   }
 }
