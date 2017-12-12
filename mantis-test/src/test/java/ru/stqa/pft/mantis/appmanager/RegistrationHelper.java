6aa6da8344b5872153d9cf7f706acf61329a0144
qmilan <qmilan911@gmail.com>
2017-12-12 22:46:02 +0400

    до мейл хелпер

--- mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -1,17 +1,20 @@
 package ru.stqa.pft.mantis.appmanager;
 
+import org.openqa.selenium.By;
 import org.openqa.selenium.WebDriver;
 
-public class RegistrationHelper {
-  private final ApplicationManager app;
-  private WebDriver wd;
+public class RegistrationHelper extends BaseHelper{
+
 
   public RegistrationHelper(ApplicationManager app) {
-    this.app=app;
+    super(app);
     wd =app.getDriver();
   }
 
   public void start(String username, String email) {
     wd.get(app.getProperty("web.baseUrl")+"/signup_page.php");
+    type(By.name("username"),username);
+    type(By.name("email"),email);
+    click(By.cssSelector("input[value='Зарегистрироваться']"));
   }
 }
