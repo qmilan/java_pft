23babd77216a601abb9e405442534684d315d055
qmilan <qmilan911@gmail.com>
2017-12-13 20:27:18 +0400

    проблема с нажатием кнопки

--- mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/appmanager/RegistrationHelper.java
@@ -17,4 +17,10 @@ public class RegistrationHelper extends BaseHelper{
     type(By.name("email"),email);
     click(By.cssSelector("input[value='Зарегистрироваться']"));
   }
+
+  public void finish(String confirmationLink, String password) {
+    wd.get(confirmationLink);
+    type(By.xpath("//*[@id=\"login-box\"]/div/div/div[4]/a"),password);
+    click(By.xpath("//*[@id=\"account-update-form\"]/fieldset/span/button/span"));
+  }
 }
