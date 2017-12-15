e87a531226d5d0b585cd330880ca265ed44f0c27
qmilan <qmilan911@gmail.com>
2017-12-15 22:41:39 +0400

    создание issue

--- mantis-test/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
+++ mantis-test/src/test/java/ru/stqa/pft/mantis/tests/SoapTests.java
@@ -1,6 +1,8 @@
 package ru.stqa.pft.mantis.tests;
 
+import org.testng.Assert;
 import org.testng.annotations.Test;
+import ru.stqa.pft.mantis.model.Issue;
 import ru.stqa.pft.mantis.model.Project;
 
 import javax.xml.rpc.ServiceException;
@@ -16,5 +18,13 @@ public class SoapTests extends TestBase {
     for (Project project : projects){
       System.out.println(project.getName());
     }
+  }
+  @Test
+public void testCreateIssue () throws RemoteException, ServiceException, MalformedURLException {
+    Set<Project> projects = app.soap().getProjects();
+    Issue issue = new Issue().withSummary("Test issue").withDescription("Test issue description")
+            .withProject(projects.iterator().next());
+    Issue created =  app.soap().addIssue(issue);
+    Assert.assertEquals(issue.getSummary(),created.getSummary());
   }
 }
